0e71ecc0d7ed2555de6cf808027b86b9188854d1
SARVESS <sarveshsri76@gmail.com>
2020-09-27 03:33:18 +0530

    Filter With Options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 import Datatable from './Datatable';
 
-require("es6-promise").polyfill();
-require("isomorphic-fetch");
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 function App() {
   const [data, setData] = useState([]);
-  const [q, setQ] = useState("");
+  const [q, setQ] = useState('');
+  const [searchColumns, setSearchColumns] = useState(['firstName', 'lastName']);
 
   useEffect(() => {
-    fetch("https://devmentor.live/api/examples/contacts?api_key=6097e5d5")
+    fetch('https://devmentor.live/api/examples/contacts?api_key=6097e5d5')
       .then((response) => response.json())
       .then((json) => setData(json));
   }, []);
 
+  function search(rows) {
+    return rows.filter((row) =>
+      searchColumns.some(
+        (column) =>
+          row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
+      )
+    );
+  }
+
+  const columns = data[0] && Object.keys(data[0]);
   return (
     <div className="App">
-      <div>filter goes here</div>
       <div>
-        <Datatable data={data}/>
+        <input type="text" value={q} onChange={(e) => setQ(e.target.value)} />
+        {columns &&
+          columns.map((column) => (
+            <label>
+              <input
+                type="checkbox"
+                checked={searchColumns.includes(column)}
+                onChange={(e) => {
+                  const checked = searchColumns.includes(column);
+                  setSearchColumns((prev) =>
+                    checked
+                      ? prev.filter((sc) => sc !== column)
+                      : [...prev, column]
+                  );
+                }}
+              />
+              {column}
+            </label>
+          ))}
+      </div>
+      <div>
+        <Datatable data={search(data)} />
       </div>
     </div>
   );
